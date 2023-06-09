3f3990a4aa08dbf3ea9371338c2529454ca518b7
Sawant Infotech <110760961+sawant-infotech@users.noreply.github.com>
2023-06-09 19:17:22 +0530

    Update index.js right click and developer option disabled
    right click and developer option disabled

--- source/js/index.js
+++ source/js/index.js
@@ -9,3 +9,37 @@ $(document).ready(function () {
     });
 });
 
+
+// disable right click
+        document.addEventListener("DOMContentLoaded", function () {
+            var bodyElement = document.querySelector("body");
+
+            bodyElement.addEventListener("contextmenu", function (event) {
+                event.preventDefault();
+                alert("Right click disabled.");
+            });
+        });
+
+        // disable copy
+        document.addEventListener("DOMContentLoaded", function () {
+            document.addEventListener("selectstart", function (event) {
+                event.preventDefault();
+            });
+
+            document.addEventListener("copy", function (event) {
+                event.preventDefault();
+            });
+        });
+
+        document.onkeydown = function (e) {
+            if ((e.ctrlKey && e.shiftKey && e.keyCode === 73) ||  // Ctrl+Shift+I
+                (e.ctrlKey && e.keyCode === 67) ||  // Ctrl+C
+                (e.ctrlKey && e.keyCode === 86) ||  // Ctrl+V
+                (e.ctrlKey && e.keyCode === 85) ||  // Ctrl+U
+                (e.ctrlKey && e.keyCode === 117)) { // Ctrl+F6
+                alert('Not allowed');
+                return false;
+            } else {
+                return true;
+            }
+        };
